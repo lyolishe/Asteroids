28d6bb442377f2f82a242c79a53af3877a67630e
Artem Strelbitskiy <artstrelb@yandex.ru>
2021-02-08 16:39:27 +0500

    ts-node добавил и поправил конфиг ssr webpack

---
+++ webpack/config/ssr.config.ts
@@ -0,0 +1,75 @@
+import { join } from 'path';
+import { Configuration } from 'webpack';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import webpackNodeExternals from 'webpack-node-externals';
+
+const fileRegex = /^(?!.*\.inline).*\.(svg|jpe?g|png|gif|eot|woff2?|ttf)$/;
+
+const config: Configuration = {
+  name: 'ssr_bundles',
+  target: 'node',
+  devtool: 'source-map',
+  entry: join('../../', 'client', 'bundles', 'index.ts'),
+  node: { __dirname: false },
+  mode: 'development',
+
+  externals: [webpackNodeExternals({
+            allowlist: [
+                /\.(?!(?:jsx?|json)$).{1,5}$/i,
+            ],
+        })],
+
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+    plugins: [
+      new TsconfigPathsPlugin(),
+    ],
+  },
+  output: {
+    filename: 'ssr.bundles.js',
+    libraryTarget: 'commonjs2',
+    path: join('../../', 'dist'),
+    publicPath: '/static/',
+  },
+
+  /*stats: {
+      all: undefined,
+      builtAt: false,
+      chunks: false,
+      assets: false,
+      errors: true,
+      warnings: true,
+      outputPath: "",
+      timings: true,
+  },*/
+
+  performance: {
+    hints: false,
+  },
+
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: { transpileOnly: true },
+      },
+      {
+        test: fileRegex,
+        loader: 'null-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'null-loader',
+      },
+      {
+        test: /\.less/,
+        loader: 'null-loader',
+      },
+    ],
+  },
+};
+
+export default [config];
